71ccab5ac3ebf69f7cb0a886aac91af074687227
Mordress <Mordress@gmail.com>
2015-11-05 16:06:21 +0300

    oh my god

--- src/com/gmail/mordress/lab2/Main.java
+++ src/com/gmail/mordress/lab2/Main.java
@@ -1,22 +1,94 @@
 package com.gmail.mordress.lab2;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         File bookFile = new File("resources" + File.separator + "sometext.txt");
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(bookFile));
-            System.out.println((char)bufferedReader.read());
-            
 
-        } catch (Exception e) {
-            e.printStackTrace();
+
+        ArrayList<String> listOfWords = new ArrayList<>();
+        ArrayList<String> listOfProffers = new ArrayList<>();
+
+        Scanner scanner = new Scanner(bookFile);
+
+        while (scanner.hasNextLine()) {
+            String[] s = scanner.nextLine().split("\\s+");
+            for (String s1 : s) {
+                listOfWords.add(s1);
+            }
+        }
+
+        /*for (String s : listOfWords) {
+            System.out.println(s);
+        }*/
+        String temp = new String();
+        //Pattern pattern = Pattern.compile("[\\.!?]+");
+        for (String iter : listOfWords) {
+            temp += " " + iter;
+            if (temp.charAt(temp.length() - 1) == '.' || temp.charAt(temp.length() - 1) == '?' || temp.charAt(temp.length() - 1) == '!') {
+                listOfProffers.add(temp);
+                temp = "";
+            }
+        }
+        for (String listOfProffer : listOfProffers) {
+            System.out.println(listOfProffer);
         }
 
 
 
+
+     /*   ArrayList<String> list = new ArrayList<>();
+
+        Pattern pattern = Pattern.compile("[А-Я]+.*[\\.!?]+\\s+");
+        //Pattern pattern = Pattern.compile("[А-ЯA-Z]((т.п.|т.д.|пр.)|[^?!.\\(]|\\([^\\)]*\\))*[.?!]");
+
+        Scanner scanner = new Scanner(bookFile);
+        String s = "";
+        while (scanner.hasNextLine()) {
+            s += scanner.nextLine();
+            Matcher matcher = pattern.matcher(s);
+
+            while (matcher.matches()) {
+                list.add(matcher.group());
+
+                s = s.substring(matcher.end()-1);
+
+            }
+
+        }
+        for (String string : list) {
+            System.out.println(string);
+            System.out.println("* * *");
+        }*/
+
+
+
+
+
+        /*Scanner scanner = new Scanner(bookFile);
+        scanner.useDelimiter(pattern);
+        while (scanner.hasNext()) {
+            System.out.println(scanner.next());
+            System.out.println("* * *");
+        }*/
+
+
+
+
+
+
+        /*Предложения разбиваются с помощью:
+
+        ([А-ЯA-Z]((т.п.|т.д.|пр.)|[^?!.\(]|\([^\)]*\))*[.?!])
+        ((\d+\.\s*)*[А-ЯA-Z]((т.п.|т.д.|пр.)|[^?!.\(]|\([^\)]*\))*[.?!])
+        Только если любые сокращения, кроме "т.д.", "т.п." и "пр." будут встречаться только в скобках.*/
 
 
 
